1bc991b3e5bded5296bece7773ebca379cfd70cc
cookie-s <cookie-s@users.noreply.github.com>
2017-11-12 22:57:49 +0900

    hello

--- src/hello.go
+++ src/hello.go
@@ -1,14 +1,26 @@
+// +build appengine
 package main
 
 import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
 )
+
+func init() {
+	e.GET("/hello", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
+}
 
 func createMux() *echo.Echo {
 	e := echo.New()
 
-	http.Handle("/", e)
+	s := standard.New("")
+	s.SetHandler(e)
+
+	http.Handle("/", s)
+
 	return e
 }
